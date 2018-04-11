92e248137f7a657fcb2a70487aeb72171d954f4d
João Belo <jpasbelo@gmail.com>
2018-04-11 20:22:26 +0100

    working (before sass)

--- app/src/about/About.js
+++ app/src/about/About.js
@@ -19,10 +19,10 @@ export default class About extends Component {
         <p>At the moment, I'm working at 327 Creative Studio, a design studio based in Porto, where we focus on creating great brands.</p>
         <p>I still make some stuff on my own from time to time, so if you have a great idea for a project, send me an email. I'm always available to give you a feedback and work together to make something awsome.</p>
         <div className="social">
-          <a target="_blank" href="mailto:mail@joaobelo.pt"><img src={iconEmail} alt="Email" /> mail@joaobelo.pt</a>
-          <a target="_blank" href="https://github.com/jpbelo"><img src={iconGithub} alt="Github" /> /jpbelo</a>
-          <a target="_blank" href="https://www.linkedin.com/in/joaobelo"><img src={iconLinkedin} alt="Linkedin" /> /joaobelo</a>
-          <a target="_blank" href="https://twitter.com/jpasbelo"><img src={iconTwitter} alt="Twitter" /> @jpasbelo</a>
+          <a rel="noopener noreferrer" target="_blank" href="mailto:mail@joaobelo.pt"><img src={iconEmail} alt="Email" /> mail@joaobelo.pt</a>
+          <a rel="noopener noreferrer" target="_blank" href="https://github.com/jpbelo"><img src={iconGithub} alt="Github" /> /jpbelo</a>
+          <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/joaobelo"><img src={iconLinkedin} alt="Linkedin" /> /joaobelo</a>
+          <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/jpasbelo"><img src={iconTwitter} alt="Twitter" /> @jpasbelo</a>
         </div>
       </div>
     );
