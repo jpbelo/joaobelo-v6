9af27c164308e424e83b5c4b60a753b6144f6101
João Belo <jpasbelo@gmail.com>
2018-04-13 20:56:57 +0100

    fixed touch event highlight

--- app/src/about/About.js
+++ app/src/about/About.js
@@ -15,8 +15,8 @@ export default class About extends Component {
         <p>Hi, I’m João and I’m a web developer based in Porto, Portugal.</p>
         <p>From early on, I had a special interest and curiosity about tech, that motivated me to learn some stuff on my own. My academic background is on graphic design and multimedia, so front-end was always apealing to me.</p>
         <p>As both web developer and designer, my passion is for making good looking web applications, that not only work, but feel good to use.</p>
-        <p>Right now, I’m working full-time at <a target="_blank" href="http://327.pt">327 Creative Studio</a>, a design studio based in Porto, where we focus on creating great brands.</p>
-        <p>I still make some stuff on the side, mainly with <a target="_blank" href="http://www.proto.pt">Proto</a> and <a target="_blank" href="http://manoamanoclub.com">Mano a Mano</a>, other two design studios in Porto.</p>
+        <p>Right now, I’m working full-time at <a rel="noopener noreferrer" target="_blank" href="http://327.pt">327 Creative Studio</a>, a design studio based in Porto, where we focus on creating great brands.</p>
+        <p>I still make some stuff on the side, mainly with <a rel="noopener noreferrer" target="_blank" href="http://www.proto.pt">Proto</a> and <a rel="noopener noreferrer" target="_blank" href="http://manoamanoclub.com">Mano a Mano</a>, other two design studios in Porto.</p>
         <p>I’m always looking for people to collaborate with, so if you have a great idea for a project, send me an email. I’d be happy to give you feedback and work together to create something awsome.</p>
         <div className="social">
           <a rel="noopener noreferrer" target="_blank" href="mailto:mail@joaobelo.pt"><img src={iconEmail} alt="Email" /> mail@joaobelo.pt</a>
