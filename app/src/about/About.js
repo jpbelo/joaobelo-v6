3f8c03b59f40ff89d264c84621d739bbefe1c8c2
João Belo <jpasbelo@gmail.com>
2018-10-20 00:35:38 +0100

    updated about text

--- app/src/about/About.js
+++ app/src/about/About.js
@@ -12,12 +12,12 @@ export default class About extends Component {
   render() {
     return (
       <div className="aboutContent">
-        <p>Hi, I’m João and I’m a web developer based in Porto, Portugal.</p>
+        <p>Hi, I’m João and I’m a FE Developer based in Porto, Portugal.</p>
         <p>From early on, I had a special interest and curiosity about tech, that motivated me to learn some stuff on my own. My academic background is on graphic design and multimedia, so front-end was always apealing to me.</p>
-        <p>As both web developer and designer, my passion is for making good looking web applications, that not only work, but feel good to use.</p>
-        <p>Right now, I’m working full-time at <a rel="noopener noreferrer" target="_blank" href="http://327.pt">327 Creative Studio</a>, a design studio based in Porto, where we focus on creating great brands.</p>
-        <p>I still make some stuff on the side, mainly with <a rel="noopener noreferrer" target="_blank" href="http://www.proto.pt">Proto</a> and <a rel="noopener noreferrer" target="_blank" href="http://manoamanoclub.com">Mano a Mano</a>, other two design studios in Porto.</p>
-        <p>I’m always looking for people to collaborate with, so if you have a great idea for a project, send me an email. I’d be happy to give you feedback and work together to create something awsome.</p>
+        <p>As both developer and designer, my passion is for making good looking interfaces, that not only work, but feel good to use.</p>
+        <p>Right now, I’m working full-time at <a rel="noopener noreferrer" target="_blank" href="http://mindera.com">Mindera</a> and <a rel="noopener noreferrer" target="_blank" href="https://www.net-a-porter.com">Net-a-Porter</a>, using technology to build great products.</p>
+        <p>From time to time, I still make cool stuff with other people, like <a rel="noopener noreferrer" target="_blank" href="http://www.327.pt">327</a>, <a rel="noopener noreferrer" target="_blank" href="http://www.proto.pt">Proto</a> and <a rel="noopener noreferrer" target="_blank" href="http://manoamanoclub.com">Mano a Mano</a>.</p>
+        <p>I’m always looking for new and cool stuff to get my hands on, so if you have a great idea for a project, send me an email. I’ll be happy to give you some feedback and work together to create something awsome.</p>
         <div className="social">
           <a rel="noopener noreferrer" target="_blank" href="mailto:mail@joaobelo.pt"><img src={iconEmail} alt="Email" /> mail@joaobelo.pt</a>
           <a rel="noopener noreferrer" target="_blank" href="https://github.com/jpbelo"><img src={iconGithub} alt="Github" /> /jpbelo</a>
