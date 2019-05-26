b55106a77583e9aeaed9ae8dd398d9c183b39ac5
João Belo <jpasbelo@gmail.com>
2019-05-27 00:18:59 +0200

    fix text

--- src/About/index.js
+++ src/About/index.js
@@ -17,7 +17,7 @@ export default class About extends Component {
         <p>As both developer and designer, my passion is for making good looking interfaces, that not only work, but feel good to use.</p>
         <p>At this moment, I’m working full-time at <a rel="noopener noreferrer" target="_blank" href="http://mindera.com">Mindera</a>, using technology to build great products for clients like <a rel="noopener noreferrer" target="_blank" href="https://net-a-porter.com">Net-a-Porter</a> and <a rel="noopener noreferrer" target="_blank" href="https://waitrose.com">Waitrose</a>.</p>
         <p>From time to time, I still make cool stuff with other people, like <a rel="noopener noreferrer" target="_blank" href="http://www.327.pt">327</a>, <a rel="noopener noreferrer" target="_blank" href="http://www.proto.pt">Proto</a> and <a rel="noopener noreferrer" target="_blank" href="http://manoamanoclub.com">Mano a Mano</a>.</p>
-        <p>I’m always looking for new and cool stuff to get my hands on, so if you have a great idea for a project, send me an email. I’ll be happy to give you some feedback and work together to create something awsome.</p>
+        <p>I’m always looking for new and cool stuff to get my hands on, so if you have a great idea for a project, send me an email. I’ll be happy to give you some feedback and work together to create something awesome.</p>
         <div className="social">
           <a rel="noopener noreferrer" target="_blank" href="mailto:joaobelo.dev@gmail.com"><img src={iconEmail} alt="Email" /> joaobelo.dev@gmail.com</a>
           <a rel="noopener noreferrer" target="_blank" href="https://github.com/jpbelo"><img src={iconGithub} alt="Github" /> /jpbelo</a>
