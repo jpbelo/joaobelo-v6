c7dedb4c3a02caed978e0785df9d70a157b798e6
João Belo <joaobelo.dev@gmail.com>
2020-03-11 16:03:02 +0000

    update experience and about

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -37,60 +37,117 @@ export default class About extends Component {
   render() {
     return (
       <Container>
-        <p>Hi, I’m João and I’m a FE Developer based in Porto, Portugal.</p>
+        <p>Hi, I’m João and I’m a Software Developer.</p>
         <p>
-          From early on, I had a special interest and curiosity about tech, that motivated me to learn some stuff on my own. My academic background is on
-          graphic design and multimedia, so front-end was always apealing to me.
+          From early on, I had a special interest and curiosity about tech, that
+          motivated me to figure how things work on my own. My academic
+          background is on graphic design and multimedia, so front-end was
+          always apealing to me.
         </p>
-        <p>As both developer and designer, my passion is for making good looking interfaces, that not only work, but feel good to use.</p>
         <p>
-          At this moment, I’m working full-time at{' '}
-          <a rel="noopener noreferrer" target="_blank" href="http://mindera.com">
+          As both developer and designer, my passion is for making good looking
+          interfaces, that not only work, but feel good to use.
+        </p>
+        <p>
+          At the moment, I'm working full-time as Tech Lead at{' '}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="http://hashdash.com"
+          >
+            HashDash
+          </a>
+          , building an exciting new service.
+        </p>
+        <p>
+          Before that I worked at{' '}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="http://mindera.com"
+          >
             Mindera
           </a>
           , using technology to build great products for clients like{' '}
-          <a rel="noopener noreferrer" target="_blank" href="https://net-a-porter.com">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://net-a-porter.com"
+          >
             Net-a-Porter
           </a>{' '}
           and{' '}
-          <a rel="noopener noreferrer" target="_blank" href="https://waitrose.com">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://waitrose.com"
+          >
             Waitrose
           </a>
           .
         </p>
         <p>
-          From time to time, I still make cool stuff with other people, like{' '}
-          <a rel="noopener noreferrer" target="_blank" href="http://www.327.pt">
-            327
+          Over the last years I've worked with some amazing people like{' '}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="http://manoamanoclub.com"
+          >
+            Mano a Mano
           </a>
           ,{' '}
-          <a rel="noopener noreferrer" target="_blank" href="http://www.proto.pt">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="http://www.proto.pt"
+          >
             Proto
           </a>{' '}
           and{' '}
-          <a rel="noopener noreferrer" target="_blank" href="http://manoamanoclub.com">
-            Mano a Mano
+          <a rel="noopener noreferrer" target="_blank" href="http://www.327.pt">
+            327
           </a>
           .
         </p>
         <p>
-          I’m always looking for new and cool stuff to get my hands on, so if you have a great idea for a project, send me an email. I’ll be happy to give you
-          some feedback and work together to create something awesome.
+          For the first time in years, I'm not working on any side projects so I
+          can keep my focus in my current position, but whenever I can, I enjoy
+          contributing to open source projects.
         </p>
         <div className="social">
-          <a rel="noopener noreferrer" target="_blank" href="mailto:joaobelo.dev@gmail.com">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:joaobelo.dev@gmail.com"
+          >
             <img src={iconEmail} alt="Email" /> joaobelo.dev@gmail.com
           </a>
-          <a rel="noopener noreferrer" target="_blank" href="https://github.com/jpbelo">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/jpbelo"
+          >
             <img src={iconGithub} alt="Github" /> /jpbelo
           </a>
-          <a rel="noopener noreferrer" target="_blank" href="https://bit.dev/joaobelo">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://bit.dev/joaobelo"
+          >
             <img src={iconBit} alt="Bit" /> /joaobelo
           </a>
-          <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/joaobelo">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/joaobelo"
+          >
             <img src={iconLinkedin} alt="Linkedin" /> /joaobelo
           </a>
-          <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/jpasbelo">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/jpasbelo"
+          >
             <img src={iconTwitter} alt="Twitter" /> @jpasbelo
           </a>
         </div>
--- src/containers/Experience/data.js
+++ src/containers/Experience/data.js
@@ -1,12 +1,29 @@
 export default [
   {
-    id: 4,
-    dateStart: '2019-04-19',
+    id: 6,
+    dateStart: '2020-02-01',
     dateEnd: 'present',
+    company: 'HashDash',
+    url: 'https://hashdash.com',
+    info:
+      'Tech Lead at <a href="https://hashdash.com" target="_blank">HashDash</a>.',
+    techs: [
+      { name: 'NextJS', url: 'https://nextjs.org/' },
+      { name: 'Redux', url: 'https://redux.js.org/' },
+      { name: 'styled-components', url: 'https://styled-components.com/' },
+      { name: 'Jest', url: 'https://jestjs.io/' },
+      { name: 'Jenkins', url: 'https://jenkins.io/' },
+      { name: 'Storybook', url: 'https://storybook.js.org/' },
+    ],
+  },
+  {
+    id: 5,
+    dateStart: '2019-04-19',
+    dateEnd: '2020-02-26',
     company: 'Waitrose',
     url: 'https://waitrose.com',
     info:
-      '(<a href="https://mindera.com" target="_blank">Mindera</a>) Working as FE developer on <a href="https://waitrose.com" target="_blank">Waitrose‘s website</a>.',
+      '(<a href="https://mindera.com" target="_blank">Mindera</a>) FE developer on <a href="https://waitrose.com" target="_blank">Waitrose‘s website</a>.',
     techs: [
       { name: 'ReactJS', url: 'https://reactjs.org/' },
       { name: 'Redux', url: 'https://redux.js.org/' },
@@ -19,13 +36,13 @@ export default [
     ],
   },
   {
-    id: 3,
+    id: 4,
     dateStart: '2018-06-11',
     dateEnd: '2019-04-14',
     company: 'NAP',
     url: 'https://net-a-porter.com',
     info:
-      '(<a href="https://mindera.com" target="_blank">Mindera</a>) Working as FE developer on <a href="https://net-a-porter.com" target="_blank">Net-a-Porter</a> and <a href="https://www.mrporter.com" target="_blank">Mr Porter</a> websites.',
+      '(<a href="https://mindera.com" target="_blank">Mindera</a>) FE developer on <a href="https://net-a-porter.com" target="_blank">Net-a-Porter</a> and <a href="https://www.mrporter.com" target="_blank">Mr Porter</a> projects.',
     techs: [
       { name: 'ReactJS', url: 'https://reactjs.org/' },
       { name: 'Redux', url: 'https://redux.js.org/' },
@@ -37,16 +54,30 @@ export default [
       { name: 'Lerna', url: 'https://lerna.js.org/' },
       { name: 'WebdriverIO', url: 'https://webdriver.io/' },
       { name: 'Cypress', url: 'https://www.cypress.io/' },
-      { name: 'WCS', url: 'https://www.ibm.com/pt-en/marketplace/websphere-commerce' },
+      {
+        name: 'WCS',
+        url: 'https://www.ibm.com/pt-en/marketplace/websphere-commerce',
+      },
       { name: 'CoreMedia CMS', url: 'https://www.coremedia.com/en/' },
     ],
+  },
+  {
+    id: 3,
+    dateStart: '2018-06-11',
+    dateEnd: '2020-02-26',
+    company: 'Mindera',
+    url: 'https://mindera.com',
+    info:
+      'Front-end developer at <a href="https://mindera.com" target="_blank">Mindera</a>, working on multiple projects.',
+    techs: [],
   },
   {
     id: 2,
     dateStart: '2014-08-04',
     dateEnd: '2018-06-01',
     company: '327 CS',
-    info: 'Web development and multimedia content for multiple clients at <a target="_blank" href="http://327.pt">327 Creative Studio</a>.',
+    info:
+      'Web development and multimedia content for multiple clients at <a target="_blank" href="http://327.pt">327 Creative Studio</a>.',
     techs: [
       { name: 'HTML' },
       { name: 'CSS' },
@@ -64,15 +95,15 @@ export default [
   },
   {
     id: 1,
-    dateStart: '2013-06-01',
-    dateEnd: 'present',
+    dateStart: '2012-06-01',
+    dateEnd: '2020-01-31',
     company: 'Freelancer',
     info:
       'Over the years I‘ve worked on a bunch of projects, some are experiments as a way to study and ' +
       'learn new tech, but most are collaborations with design studios and colleges from college. ' +
       'Some of those are under the My Work tab.<br/>' +
       'To keep things simple, I won‘t be listing technologies here, but I‘m currently focused on Javascript ' +
-      'frameworks and thing that go hand-to-hand with them, like styled-components, storybook and so on.',
+      'frameworks and thing that go hand-to-hand with them.',
     techs: [],
   },
 ]
